2d48a353903baf1473592cec00fa67e48c46b60c
Luis <machadoadriano28@gmail.com>
2025-05-07 20:21:41 -0500

    Inicializando Repositorio DinnraWebApi

---
+++ src/test/java/com/example/DinnraWeb/DinnraWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.DinnraWeb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DinnraWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
